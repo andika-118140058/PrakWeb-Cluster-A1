3e2ba9186afb79421a569b28dbbc8d1dccf14b52
andika-118140058 <andikas926@gmail.com>
2020-12-02 05:19:46 +0700

    commit

---
+++ Modul6/Latihan/Cookies/hapus_cookies.php
@@ -0,0 +1,11 @@
+<?php
+    setcookie("user","",time()-3600);
+?>
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            echo "Cookie 'user' terhapus.";
+        ?>
+    </body>
+</html>
---
+++ Modul6/Latihan/Session/home.php
@@ -0,0 +1,32 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['user'])||(trim($_SESSION['user'])=='')){
+        header('location:index.php');
+    }
+    include_once('user.php');
+    $user=new User();
+    //fetch user data
+    $sql="select * from user where id=".$_SESSION['user']."";
+    $row = $user->details($sql);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Login</title>
+    </head>
+    <body>
+        <div>
+            <h1>PHP Login</h1>
+            <div>
+                <div>
+                    <h2>Selamat datang di Dashboard</h2>
+                    <h4>User Info: </h4>
+                    <p>Name : <?=$row['fname'];?></p>
+                    <p>Username : <?=$row['username'];?></p>
+                    <p>Password : <?=$row['password'];?></p>
+                    <a href="logout.php"><span></span>Logout</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
